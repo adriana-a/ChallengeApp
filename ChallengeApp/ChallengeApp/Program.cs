929c6381431dc131b406b7a94bd7ab08b9021c5f
Adriana Kopeć <adriana.kopec8@gmail.com>
2023-04-06 14:04:20 +0200

    Dzień 4 - zadanie

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Ewa";
+var isFemale = true;
+var age = 29;
+
+if (isFemale && age < 30)
+{
+    Console.WriteLine("Oto kobieta poniżej 30 lat");
+}
+
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("A to Ewa, która ma 33 lata");
+}
+
+else if (isFemale = false && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+
